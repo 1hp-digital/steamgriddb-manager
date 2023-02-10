5a73847f9c5b4c4e0bec86ee9746eeca1144f177
Sarah Day <235341+aranel616@users.noreply.github.com>
2023-02-09 21:52:33 -0800

    Changing Search.jsx to a functional component

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import Image from "react-uwp/Image";
@@ -7,30 +7,25 @@ import PubSub from "pubsub-js";
 import TopBlur from "./TopBlur";
 import Spinner from "./Spinner";
 import Steam from "../utils/Steam";
+import { getTheme } from "react-uwp/Theme";
 
-const SGDB = window.require("steamgriddb");
+const SteamGridDB = window.require("steamgriddb");
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props);
+const Search = (props) => {
+    const { location } = props;
+    console.log(SteamGridDB);
 
-        this.onClick = this.onClick.bind(this);
-        this.SGDB = new SGDB("b971a6f5f280490ab62c0ee7d0fd1d16");
+    const SGDB = new SteamGridDB("b971a6f5f280490ab62c0ee7d0fd1d16");
+    const game = location.state;
 
-        const { location } = this.props;
+    const [items, setItems] = useState([]);
+    const [redirect, setRedirect] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-        this.state = {
-            game: location.state,
-            items: [],
-            toGame: false,
-            isLoaded: false,
-        };
+    const theme = getTheme();
 
+    useEffect(() => {
         PubSub.publish("showBack", true);
-    }
-
-    componentDidMount() {
-        const { game } = this.state;
 
         let type = "steam";
         if (game.platform) {
@@ -46,120 +41,94 @@ class Search extends React.Component {
 
         if (game.platform === "other") {
             type = "game";
-            this.SGDB.searchGame(game.name)
+            SGDB.searchGame(game.name)
                 .then((gameResp) => {
                     id = gameResp[0].id;
-                    this.queryApi(type, id);
+                    queryApi(type, id);
                 });
         } else {
-            this.queryApi(type, id);
+            queryApi(type, id);
         }
-    }
+    }, []);
 
-    onClick(item, itemIndex) {
-        const { game, items } = this.state;
-        const { location } = this.props;
-
+    const onClick = (item, itemIndex) => {
         const clonedItems = [...items];
         clonedItems[itemIndex].downloading = true;
 
-        this.setState({
-            items: clonedItems,
-        });
+        setItems(clonedItems);
 
         Steam.addAsset(location.state.assetType, game.appid, item.url).then(() => {
             clonedItems[itemIndex].downloading = false;
-            this.setState({
-                items: clonedItems,
-            });
-            this.setState({ toGame: <Redirect to={{ pathname: "/game", state: location.state }} /> });
+            setItems(clonedItems);
+            setRedirect(<Redirect to={{ pathname: "/game", state: location.state }} />);
         });
-    }
+    };
 
-    queryApi(type, id) {
-        const { location } = this.props;
-
+    const queryApi = (type, id) => {
         switch (location.state.assetType) {
             case "horizontalGrid":
-                this.SGDB.getGrids({ type, id }).then((res) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: res,
-                    });
+                SGDB.getGrids({ type, id }).then((res) => {
+                    setIsLoaded(true);
+                    setItems(res);
                 });
+
                 break;
             case "verticalGrid":
-                this.SGDB.getGrids({ type, id, dimensions: ["600x900"] }).then((res) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: res,
-                    });
+                SGDB.getGrids({ type, id, dimensions: ["600x900"] }).then((res) => {
+                    setIsLoaded(true);
+                    setItems(res);
                 });
+
                 break;
             case "hero":
-                this.SGDB.getHeroes({ type, id }).then((res) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: res,
-                    });
+                SGDB.getHeroes({ type, id }).then((res) => {
+                    setIsLoaded(true);
+                    setItems(res);
                 });
+
                 break;
             case "logo":
-                this.SGDB.getLogos({ type, id }).then((res) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: res,
-                    });
+                SGDB.getLogos({ type, id }).then((res) => {
+                    setIsLoaded(true);
+                    setItems(res);
                 });
+
                 break;
             default:
                 break;
         }
+    };
+
+    if (!isLoaded) {
+        return <Spinner />;
     }
 
-    render() {
-        const { isLoaded, toGame, items } = this.state;
-        const { theme } = this.context;
+    if (redirect) {
+        return redirect;
+    }
 
-        if (!isLoaded) {
-            return <Spinner />;
-        }
-
-        if (toGame) {
-            return toGame;
-        }
-
-        return (
-            <>
-                <TopBlur />
-                <div
-                    id="search-container"
-                    style={{
-                        height: "100%",
-                        overflow: "auto",
-                        padding: 15,
-                        paddingLeft: 10,
-                        paddingTop: 45,
-                    }}
-                >
-                    {items.map((item, i) => (
-                        <Button
-                            key={item.id}
-                            style={{ padding: 0, margin: 5 }}
-                            onClick={() => this.onClick(item, i)}
-                        >
-                            {item.downloading ? (
-                                <div style={{ position: "relative" }}>
-                                    <Spinner size={70} style={{ position: "absolute", background: "rgba(0,0,0,.5)" }} />
-                                    <Image
-                                        style={{
-                                            width: "100%",
-                                            height: "auto",
-                                        }}
-                                        src={item.thumb}
-                                    />
-                                </div>
-                            ) : (
+    return (
+        <>
+            <TopBlur />
+            <div
+                id="search-container"
+                style={{
+                    height: "100%",
+                    overflow: "auto",
+                    padding: 15,
+                    paddingLeft: 10,
+                    paddingTop: 45,
+                }}
+            >
+                {items.map((item, i) => (
+                    <Button
+                        key={item.id}
+                        style={{ padding: 0, margin: 5 }}
+                        onClick={() => onClick(item, i)}
+                    >
+                        {item.downloading ? (
+                            <div style={{ position: "relative" }}>
+                                <Spinner size={70} style={{ position: "absolute", background: "rgba(0,0,0,.5)" }} />
                                 <Image
                                     style={{
                                         width: "100%",
@@ -167,21 +136,31 @@ class Search extends React.Component {
                                     }}
                                     src={item.thumb}
                                 />
-                            )}
-                            <p style={{ ...theme.typographyStyles.captionAlt, padding: 5 }}>
-                                <Image style={{ height: 20, marginRight: 5 }} src={item.author.avatar} />
-                                {item.author.name}
-                            </p>
-                        </Button>
-                    ))}
-                </div>
-            </>
-        );
-    }
-}
+                            </div>
+                        ) : (
+                            <Image
+                                style={{
+                                    width: "100%",
+                                    height: "auto",
+                                }}
+                                src={item.thumb}
+                            />
+                        )}
+                        <p style={{ ...theme.typographyStyles.captionAlt, padding: 5 }}>
+                            <Image style={{ height: 20, marginRight: 5 }} src={item.author.avatar} />
+                            {item.author.name}
+                        </p>
+                    </Button>
+                ))}
+            </div>
+        </>
+    );
+};
 
 Search.propTypes = {
     location: PropTypes.object.isRequired,
 };
+
 Search.contextTypes = { theme: PropTypes.object };
+
 export default Search;
