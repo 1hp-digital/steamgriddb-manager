bb427949f5d679686d9ec11de845c21ea0394f4a
Sarah Day <235341+aranel616@users.noreply.github.com>
2023-02-09 21:16:54 -0800

    Changing Game.jsx to a functional component

--- app/components/Game.jsx
+++ app/components/Game.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import Image from "react-uwp/Image";
@@ -10,151 +10,133 @@ import heroPlaceholder from "../images/hero_none.png";
 import capsuleVerticalPlaceholder from "../images/capsule_vertical_none.png";
 import capsulePlaceholder from "../images/capsule_none.png";
 import logoPlaceholder from "../images/logo_none.png";
+import { getTheme } from "react-uwp/Theme";
 
-const { join } = window.require("path");
-const fs = window.require("fs");
+const Game = (props) => {
+    const {location} = props;
+    const game = location.state;
 
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toSearch = this.toSearch.bind(this);
+    const [grid, setGrid] = useState();
+    const [poster, setPoster] = useState();
+    const [hero, setHero] = useState();
+    const [logo, setLogo] = useState();
+    const [redirect, setRedirect] = useState();
 
-        const { location } = this.props;
+    const theme = getTheme();
 
-        this.state = {
-            game: location.state,
-            toSearch: false,
-            grid: null,
-            poster: null,
-            hero: null,
-            logo: null,
-        };
-
+    useEffect(() => {
         PubSub.publish("showBack", true);
-    }
-
-    componentDidMount() {
-        const { game } = this.state;
-        const self = this;
 
         Steam.getGameImages(game).then((images) => {
-            self.setState(images);
+            setGrid(images.grid);
+            setPoster(images.poster);
+            setHero(images.hero);
+            setLogo(images.logo);
         });
-    }
+    }, []);
 
-    toSearch(assetType) {
-        const { location } = this.props;
-        this.setState({ toSearch: <Redirect to={{ pathname: "/search", state: { ...location.state, assetType } }} /> });
-    }
+    const toSearch = (assetType) => {
+        setRedirect(<Redirect to={{ pathname: "/search", state: { ...location.state, assetType } }} />);
+    };
 
-    addNoCache(imageURI) {
+    const addNoCache = (imageURI) => {
         if (!imageURI) {
             return false;
         }
 
         return `${imageURI}?${(new Date().getTime())}`;
+    };
+
+    if (redirect) {
+        return redirect;
     }
 
-    render() {
-        const {
-            toSearch,
-            game,
-            grid,
-            hero,
-            poster,
-            logo,
-        } = this.state;
+    const titleStyle = {
+        // ...theme.typographyStyles.subTitle,
+        padding: "20px 0px 10px 0",
+        width: "100%",
+    };
+    const buttonStyle = {
+        padding: 0,
+    };
 
-        if (toSearch) {
-            return toSearch;
-        }
+    return (
+        <>
+            <TopBlur />
+            <div
+                id="search-container"
+                style={{
+                    height: "100%",
+                    overflow: "auto",
+                    padding: 15,
+                    paddingLeft: 10,
+                    paddingTop: 45,
+                }}
+            >
+                <h1 style={theme.typographyStyles.header}>{game.name}</h1>
+                <h5 style={titleStyle}>Hero</h5>
+                <Button style={buttonStyle} onClick={() => toSearch("hero")}>
+                    <Image
+                        style={{
+                            width: "100%",
+                            height: "auto",
+                        }}
+                        src={addNoCache(hero) || heroPlaceholder}
+                    />
+                </Button>
 
-        const { theme } = this.context;
-        const titleStyle = {
-            ...theme.typographyStyles.subTitle,
-            padding: "20px 0px 10px 0",
-            width: "100%",
-        };
-        const buttonStyle = {
-            padding: 0,
-        };
-
-        return (
-            <>
-                <TopBlur />
-                <div
-                    id="search-container"
-                    style={{
-                        height: "100%",
-                        overflow: "auto",
-                        padding: 15,
-                        paddingLeft: 10,
-                        paddingTop: 45,
-                    }}
-                >
-                    <h1 style={theme.typographyStyles.header}>{game.name}</h1>
-                    <h5 style={titleStyle}>Hero</h5>
-                    <Button style={buttonStyle} onClick={() => this.toSearch("hero")}>
-                        <Image
-                            style={{
-                                width: "100%",
-                                height: "auto",
-                            }}
-                            src={this.addNoCache(hero) || heroPlaceholder}
-                        />
-                    </Button>
-
-                    <div style={{ display: "flex" }}>
-                        <div style={{ flex: 1 }}>
-                            <h5 style={titleStyle}>Vertical Capsule</h5>
-                            <Button style={buttonStyle} onClick={() => this.toSearch("verticalGrid")}>
-                                <Image
-                                    style={{
-                                        maxWidth: "100%",
-                                        height: "auto",
-                                    }}
-                                    src={this.addNoCache(poster) || capsuleVerticalPlaceholder}
-                                />
-                            </Button>
-                        </div>
-                        <div
-                            style={{
-                                marginLeft: 10,
-                                flex: 1,
-                            }}
-                        >
-                            <h5 style={titleStyle}>Horizontal Capsule</h5>
-                            <Button style={buttonStyle} onClick={() => this.toSearch("horizontalGrid")}>
-                                <Image
-                                    style={{
-                                        maxWidth: "100%",
-                                        height: "auto",
-                                    }}
-                                    src={this.addNoCache(grid) || capsulePlaceholder}
-                                />
-                            </Button>
-                        </div>
-                    </div>
-                    <div>
-                        <h5 style={titleStyle}>Logo</h5>
-                        <Button style={buttonStyle} onClick={() => this.toSearch("logo")}>
+                <div style={{ display: "flex" }}>
+                    <div style={{ flex: 1 }}>
+                        <h5 style={titleStyle}>Vertical Capsule</h5>
+                        <Button style={buttonStyle} onClick={() => toSearch("verticalGrid")}>
                             <Image
                                 style={{
                                     maxWidth: "100%",
                                     height: "auto",
                                 }}
-                                src={this.addNoCache(logo) || logoPlaceholder}
+                                src={addNoCache(poster) || capsuleVerticalPlaceholder}
+                            />
+                        </Button>
+                    </div>
+                    <div
+                        style={{
+                            marginLeft: 10,
+                            flex: 1,
+                        }}
+                    >
+                        <h5 style={titleStyle}>Horizontal Capsule</h5>
+                        <Button style={buttonStyle} onClick={() => toSearch("horizontalGrid")}>
+                            <Image
+                                style={{
+                                    maxWidth: "100%",
+                                    height: "auto",
+                                }}
+                                src={addNoCache(grid) || capsulePlaceholder}
                             />
                         </Button>
                     </div>
                 </div>
-            </>
-        );
-    }
-}
+                <div>
+                    <h5 style={titleStyle}>Logo</h5>
+                    <Button style={buttonStyle} onClick={() => toSearch("logo")}>
+                        <Image
+                            style={{
+                                maxWidth: "100%",
+                                height: "auto",
+                            }}
+                            src={addNoCache(logo) || logoPlaceholder}
+                        />
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
+};
 
 Game.propTypes = {
     location: PropTypes.object.isRequired,
 };
+
 Game.contextTypes = { theme: PropTypes.object };
+
 export default Game;
